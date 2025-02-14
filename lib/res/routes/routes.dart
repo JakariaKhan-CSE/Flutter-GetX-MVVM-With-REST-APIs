f1a3d7f0fb4860ef8bd3e394eeb79f8078ab4246
Md. Jakaria Ibna Azam Khan. <mdjakariaibnaazamkhan@gmail.com>
2025-02-14 21:26:11 +0600

    app successfully work.

--- lib/res/routes/routes.dart
+++ lib/res/routes/routes.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 import 'package:getx_mvvm_with_restapi/res/routes/routes_name.dart';
+import 'package:getx_mvvm_with_restapi/view/home/home_view.dart';
 import 'package:getx_mvvm_with_restapi/view/login/login_view.dart';
 import 'package:getx_mvvm_with_restapi/view/splash_screen.dart';
 
@@ -15,25 +16,10 @@ class AppRoutes {
             page: () => LoginView(),
             transitionDuration: Duration(milliseconds: 250),
             transition: Transition.leftToRightWithFade),
+        GetPage(
+            name: RouteName.homeView,
+            page: () => HomeView(),
+            transitionDuration: Duration(milliseconds: 250),
+            transition: Transition.leftToRightWithFade),
       ];
-  // static appRoutes() => [
-  //   GetPage(
-  //     name: RouteName.splashScreen,
-  //     page: () => SplashScreen() ,
-  //     transitionDuration: Duration(milliseconds: 250),
-  //     transition: Transition.leftToRightWithFade ,
-  //   ) ,
-  //   GetPage(
-  //     name: RouteName.loginView,
-  //     page: () => LoginView() ,
-  //     transitionDuration: Duration(milliseconds: 250),
-  //     transition: Transition.leftToRightWithFade ,
-  //   ) ,
-  //   GetPage(
-  //     name: RouteName.homeView,
-  //     page: () => HomeView() ,
-  //     transitionDuration: Duration(milliseconds: 250),
-  //     transition: Transition.leftToRightWithFade ,
-  //   ) ,
-  // ];
 }
